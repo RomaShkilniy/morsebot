99c111475294934159f38fc0fecbccf69f7215bf
RomaShkilniy <romaskilnij@gmail.com>
2021-03-09 16:30:55 +0200

    F

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import telebot
 import config
+import time
 
 bot = telebot.TeleBot(config.token)
 
@@ -20,6 +21,9 @@ def welcome(message):
 	bot.send_message(message.chat.id, 'Welcome {} 😘'.format(message.chat.first_name))
 	bot.send_message(message.chat.id, 'I\'m a bot that converts text to Morse code.' )
 
+@bot.message_handler(commands=['random'])
+def random_dice(message):
+  bot.send_dice(message.chat.id)
 
 @bot.message_handler(content_types=['text'])
 def send(message):
@@ -37,6 +41,15 @@ def send(message):
 
 	bot.send_message(message.chat.id, result )
 
+@bot.message_handler(content_types=['sticker'])
+def sticker_id(message):
+  bot.send_sticker(message.chat.id, message.sticker.file_id)
+  print(message.sticker.file_id)
+
+@bot.message_handler(content_types=['dice'])
+def dice_value(message):
+  time.sleep(2.8)
+  bot.send_message(message.chat.id, f'Your value is {message.dice.value}')
 
 if __name__ == '__main__':
 	bot.polling(none_stop = True, interval = 0)
